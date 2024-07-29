a4551e4ceb068a42e1d5f99c0bed8b08c3c486c6
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-07-29 02:35:24 -0500

    comentarios y estudio de arith

--- 08_arith.py
+++ 08_arith.py
@@ -1,11 +1,42 @@
-print(10 +10)
-print(11 - 5)
-print(8 * 8)
-print(10 / 2)
-print(10 % 3)
-print(10 // 3)
-print(10 ** 2)
-print(2 ** 3 + 3 - 7 / 1 // 4)
-print(8 + 3 - 7 / 1 // 4)
-print(8 + 3 - 7 // 4)
-print(8 + 3 - 1)
+print('---------Aritmetica---------')
+
+# Inicializa las variables a y b
+a = 10
+b = 3
+
+# Imprime los valores de las variables a y b
+print('Sea a =', a)
+print('Sea b =', b)
+
+# Imprime la suma de a y b
+print('Suma de a + b =', a + b)
+
+# Imprime la resta de a y b
+print('Resta a - b =', a - b)
+
+# Imprime la multiplicación de a y b
+print('Multiplicación a * b =', a * b)
+
+# Imprime la división de a por b
+print('División a / b =', a / b)
+
+# Imprime la división entera de a por b (parte entera de la división)
+print('División entera a / b =', a // b)
+
+# Imprime el módulo de la división de a por b (resto de la división)
+print('Módulo de la división a / b =', 10 % 3)
+
+# Imprime la potencia de a elevado a b
+print('Potencia de a elevado b =', 10 ** 2)
+
+# Imprime una operación compuesta: 2 elevado a 3, más 3, menos 7, dividido entre 1, y dividido por 4 (división entera)
+print('Operación de 2 ** 3 + 3 - 7 / 1 // 4 =', 2 ** 3 + 3 - 7 / 1 // 4)
+
+# Imprime una operación compuesta: 8 más 3, menos 7, dividido entre 1, y dividido por 4 (división entera)
+print('Operación de 8 + 3 - 7 / 1 // 4 =', 8 + 3 - 7 / 1 // 4)
+
+# Imprime una operación compuesta: 8 más 3, menos 7, dividido por 4 (división entera)
+print('Operación de 8 + 3 - 7 // 4 =', 8 + 3 - 7 // 4)
+
+# Imprime una operación compuesta: 8 más 3, menos 1
+print('Operación de 8 + 3 - 1 =', 8 + 3 - 1)
